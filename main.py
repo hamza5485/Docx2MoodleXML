aa8105c0da1314abc1051792aca119ac9bc6b5a5
Hamza Anas <hamza.anas@itinnovate.com.au>
2021-01-11 11:15:09 +0500

    made naming convention less static

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ for docx_file in os.listdir(DOCX_DIR):
     q_index = 1
     if docx_file != ".gitignore":
         file_path = DOCX_DIR + docx_file
-        prefix = f"{docx_file[0:3].upper()}_{docx_file[7:9]}_Q_"
+        name_arr = docx_file.split(" ")
+        prefix = f"{name_arr[0].upper()}_{name_arr[1]}_Q_"
         for index, p in enumerate(docx.Document(file_path).paragraphs):
             if index == 0:
                 pass
@@ -36,7 +37,8 @@ for docx_file in os.listdir(DOCX_DIR):
                         content.append(opts)
 
         for i, e in enumerate(content):
-            if not (e.startswith(MULTIPLE_CHOICE) or e.startswith(TRUE_FALSE) or e.startswith("DOM")):
+            if not (e.startswith(MULTIPLE_CHOICE) or e.startswith(TRUE_FALSE) or e.startswith(
+                    f"{name_arr[0].upper()}_{name_arr[1]}_Q_")):
                 question_name = content[i - 1]
                 question_text = e
                 question_opts = {}
@@ -47,6 +49,7 @@ for docx_file in os.listdir(DOCX_DIR):
                     question_opts["B"] = content[i + 2][3:]
                     question_opts["C"] = content[i + 3][3:]
                     question_opts["D"] = content[i + 4][3:]
+                    question_opts["E"] = content[i + 5][3:]
                 elif content[i + 1].startswith(TRUE_FALSE):
                     question_type = "truefalse"
                     question_opts["A"] = content[i + 1][3:]
@@ -62,7 +65,7 @@ for docx_file in os.listdir(DOCX_DIR):
         cat_question.setAttribute("type", "category")
         category = dom.createElement("category")
         text = dom.createElement("text")
-        text.appendChild(dom.createTextNode(f"$course$/top/{docx_file[0:9]}"))
+        text.appendChild(dom.createTextNode(f"$course$/top/{name_arr[0]}"))
         category.appendChild(text)
         cat_question.appendChild(category)
         quiz_elem.appendChild(cat_question)
